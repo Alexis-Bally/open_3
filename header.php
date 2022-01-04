db7afb0b07eb59d68a68097b7a143dfcc378a2cb
Alexis Bally <alexisbally02@gmail.com>
2022-01-04 13:47:19 +0100

    :building_construction: Update all

--- header.php
+++ header.php
@@ -33,8 +33,11 @@
 				?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				<?php
+			else :
+				?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php
 			endif;
-			$open_3_description = get_bloginfo( 'description', 'display' );
 			if ( $open_3_description || is_customize_preview() ) :
 				?>
 				<p class="site-description"><?php echo $open_3_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
