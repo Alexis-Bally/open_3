e329af19ddf203fc2b558753f21ca4d24d15194f
Alexis Bally <alexisbally02@gmail.com>
2022-01-07 14:31:47 +0100

    :construction:

--- footer.php
+++ footer.php
@@ -15,24 +15,32 @@
 		<div class="footer_inner-container">
 			<div class="footer_grid">
 				<div class="footer_liens">
-					<p>Mentions légales</p>
-					<p>Conditions  d’utilisation</p>
-					<p>CGV</p>
+					<div class="footer_liens_inner-container">
+						<a href="#">Mentions légales</a>
+						<a href="#">Conditions d’utilisation</a>
+						<a href="#">CGV</a>
+					</div>
 				</div>
 				<div class="footer_logo">
 					<img src="<?php echo get_template_directory_uri();?>/img/logo-open3-blanc.png" alt="Logo Open3 blanc">
 				</div>
 				<div class="footer_infos">
 					<div class="adress">
-						<img src="<?php echo get_template_directory_uri();?>/img/lieu.png" alt="Icone lieu">
+						<div class="footer_adress_img">
+							<img src="<?php echo get_template_directory_uri();?>/img/lieu.png" alt="Icone lieu">
+						</div>
 						<p>47 boulevard de Pesaro Nanterre 92000</p>
 					</div>
 					<div class="mail">
-						<img src="<?php echo get_template_directory_uri();?>/img/mail.png" alt="Icone mail">
+						<div class="footer_mail_img">
+							<img src="<?php echo get_template_directory_uri();?>/img/mail.png" alt="Icone mail">
+						</div>
 						<p>iim@devinci.fr</p>
 					</div>
 					<div class="phone">
-						<img src="<?php echo get_template_directory_uri();?>/img/telephone.png" alt="Icone telephone">
+						<div class="footer_phone_img">
+							<img src="<?php echo get_template_directory_uri();?>/img/telephone.png" alt="Icone telephone">
+						</div>
 						<p>01 81 00 30 30</p>
 					</div>
 				</div>
